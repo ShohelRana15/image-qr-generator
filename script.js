a17fb693d20e3d922d2c91cfa85566495049a15b
MD SHOHEL RANA <132427896+ShohelRana15@users.noreply.github.com>
2026-07-26 18:07:23 +0600

    Add Image and Text Tabs

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("QR Hub Ready");
+const tabs = document.querySelectorAll(".tab");
+
+const imageSection = document.getElementById("imageSection");
+
+const textSection = document.getElementById("textSection");
+
+tabs[0].onclick = ()=>{
+
+tabs[0].classList.add("active");
+tabs[1].classList.remove("active");
+
+imageSection.style.display="block";
+textSection.style.display="none";
+
+}
+
+tabs[1].onclick = ()=>{
+
+tabs[1].classList.add("active");
+tabs[0].classList.remove("active");
+
+imageSection.style.display="none";
+textSection.style.display="block";
+
+}
