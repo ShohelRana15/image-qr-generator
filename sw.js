dcf87f463dd4575160eed2e0a30bd30d145d6fa3
MD SHOHEL RANA <132427896+ShohelRana15@users.noreply.github.com>
2026-07-29 13:04:21 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,13 @@
 const CACHE_NAME = "qrhub-v3";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  // শুধু Network থেকে লোড করবে
 });
