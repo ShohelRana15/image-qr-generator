1ba35979b05f59867504fff8c7eff96767f70142
MD SHOHEL RANA <132427896+ShohelRana15@users.noreply.github.com>
2026-07-29 11:14:41 +0600

    Update script.js

--- script.js
+++ script.js
@@ -198,7 +198,7 @@ const fileName=Date.now()+"_"+uploadFile.name;
 
 const {error}=await supabaseClient.storage
 
-.from("IMAGES")
+.from("images")
 
 .upload(fileName,uploadFile,{
 upsert:true
@@ -220,7 +220,7 @@ return;
 
 const {data}=supabaseClient.storage
 
-.from("IMAGES")
+.from("images")
 
 .getPublicUrl(fileName);
 
